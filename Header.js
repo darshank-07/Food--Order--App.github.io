b470dc67ae1b94b0c23102021555153d354476d1
Dewanand Kakpure <72249046+darshank-07@users.noreply.github.com>
2023-02-24 08:53:10 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -17,3 +17,20 @@ function Header() {
             <p>2</p>
           </div>
           </div>
+  <div className="profilecontainer">
+            <div className="imagBoX">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVi-cR4dMAo4Df1f8BZO5s7x9CdWFl_mUtVltKwIXQeazQ33HNYsgXmQzgc110pJD_PrQ&usqp=CAU" alt="" />
+            </div>
+            
+            <h className="userName"><b>Dewanand</b></h>
+        </div>
+        <div className="toggleMenu">
+        <BarChart className="toggleIcon" />
+        </div>
+       
+        
+    </header>
+  )
+}
+
+export default Header ;
