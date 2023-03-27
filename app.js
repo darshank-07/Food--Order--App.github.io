ad2b5bc54d2e4a68c9366903b2a9c6b11a79ee79
Dewanand Kakpure <72249046+darshank-07@users.noreply.github.com>
2023-03-27 12:41:12 +0530

    Update app.js
    Totally Update

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
-import { AccountBalance, CardTravel, Chat, Favorite, HomeRounded, Settings, SummarizeRounded } from '@mui/icons-material';
+import { AccountBalance, AccountBalanceWalletRounded, Chat, Favorite, HomeRounded, Settings, SummarizeRounded } from '@mui/icons-material';
+import { useEffect } from 'react';
 import './App.css';
 import Header from './component/Header';
+import MenuContainer from './component/MenuContainer';
 
 function App() {
+  useEffect(() =>  {
+    const menuLi = document.querySelectorAll("#menu Li");
+
+    function setMenuActive(){
+      menuLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+      
+    
+
+    menuLi.forEach((n) =>n.addEventListener("click",setMenuActive));
+
+  },[]);
   return (
     <div className="App">
-      {/* Header section */}
-      <Header />
 
-      {/* main container */}
+      {/* Header section*/}
+      <Header />
+      {/* Main Container*/}
+      <main>
       
-      {/* Bottom container */}
+        <div className="mainContainer">
+             {/*banner*/}
+           <div className="banner">
+              <bannerName name={"Devanad"} discount={"20"} link={"#"} />
+              <img
+              src=""
+
+              alt=""
+              className="delivarypic"
+              />
+        
+
+           </div>
+
+        </div>
+      
